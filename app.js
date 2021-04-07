d42971d3cb335e0579c196600a26a720b94d451e
sergioivanov008 <sergioivanov008@gmail.com>
2021-04-07 14:47:41 +0300

    refactor: use BEM notation according rule 2.1 (advanced level)

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var createNewTaskElement=function(taskString){
 
   //Each elements, needs appending
   checkBox.type="checkbox";
-  checkBox.classList.add("checkbox");
+  checkBox.classList.add("item__checkbox");
   editInput.type="text";
   editInput.className="task input-text";
 
