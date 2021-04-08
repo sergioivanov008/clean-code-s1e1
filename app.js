6eb4a4d5033b2e4db129ace9e464046cc9dcc321
sergioivanov008 <sergioivanov008@gmail.com>
2021-04-08 21:49:23 +0300

    refactor: use BEM notation according rule 2.1 (advanced level)

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ var editTask=function(){
   var editInput=listItem.querySelector("input[type=text]");
   var label=listItem.querySelector("label");
   var editBtn=listItem.querySelector(".item__btn-edit");
-  var containsClass=listItem.classList.contains("edit-mode");
+  var containsClass=listItem.classList.contains("item--edit-mode");
   //If class of the parent is .edit-mode
   if(containsClass){
 
@@ -104,7 +104,7 @@ var editTask=function(){
     }
 
     //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("item--edit-mode");
 };
 
 
